ff3110b8867ecef564387f1d457a5049417d954e
Sadiq Umar <umarsadiqibrahim@gmail.com>
2023-12-01 07:55:30 +0100

    Add Users Table Seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
